e15356e52de5fe6e18ebcce4a8e2c00b6dabdc1a
sujood-totah <sojoud.totah04@gmail.com>
2026-05-01 12:09:59 +0300

    fix: remove submodule

---
+++ another_project_dir/operating-systems-project/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int add_numbers(int a, int b){
+    return a+b;
+}
+
+int main(void) {
+    printf("Hello, World!\n");
+    int a=3;
+    int b=4;
+    add_numbers(a,b);
+    int i;
+    return 0;
+}
